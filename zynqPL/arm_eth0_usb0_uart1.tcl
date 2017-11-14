8803676ad1ddc1ae027d93139e39eab1f83e4a5f
Richard Nicolet <rnicolet@logitech.com>
2017-11-14 11:15:07 -0800

    Add gitignore. Script are called from zynqPL sub-folder

--- zynqPL/arm_eth0_usb0_uart1.tcl
+++ zynqPL/arm_eth0_usb0_uart1.tcl
@@ -1,9 +1,9 @@
 #!/usr/bin/tclsh
 
-# set top_folder "/home/rnicolet/workspace/microzed"
+# We are running the script from .../microzed/zynqPL folder.
 set top_folder "."
 
-create_project zynqPL $top_folder/zynqPL -part xc7z010clg400-1
+create_project zynqPL $top_folder -part xc7z010clg400-1
 
 set_property board_part em.avnet.com:microzed_7010:part0:1.1 [current_project]
 create_bd_design "design_zynq"
@@ -18,13 +18,13 @@ startgroup
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
 endgroup
 
-make_wrapper -files [get_files $top_folder/zynqPL/zynqPL.srcs/sources_1/bd/design_zynq/design_zynq.bd] -top
+make_wrapper -files [get_files $top_folder/zynqPL.srcs/sources_1/bd/design_zynq/design_zynq.bd] -top
 
-add_files -norecurse $top_folder/zynqPL/zynqPL.srcs/sources_1/bd/design_zynq/hdl/design_zynq_wrapper.v
+add_files -norecurse $top_folder/zynqPL.srcs/sources_1/bd/design_zynq/hdl/design_zynq_wrapper.v
 update_compile_order -fileset sources_1
 update_compile_order -fileset sim_1
 launch_runs impl_1 -to_step write_bitstream -jobs 8
 
 
-file mkdir $top_folder/zynqPL/zynqPL.sdk
-file copy -force $top_folder/zynqPL/zynqPL.runs/impl_1/design_zynq_wrapper.sysdef /home/rnicolet/workspace/microzed/zynqPL/zynqPL.sdk/design_zynq_wrapper.hdf
+file mkdir $top_folder/zynqPL.sdk
+file copy -force $top_folder/zynqPL.runs/impl_1/design_zynq_wrapper.sysdef /home/rnicolet/workspace/microzed/zynqPL/zynqPL.sdk/design_zynq_wrapper.hdf
